c10ea9d4db7e372055951545c9d52c968481acb3
Santosh190512 <santoshbehera34514@gmail.com>
2026-08-29 15:41:28 +0530

    added postgresql support

--- ganesh_puja_project/settings.py
+++ ganesh_puja_project/settings.py
@@ -70,15 +70,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ganesh_puja_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import dj_database_url
+import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
+
 
 
 # Password validation
