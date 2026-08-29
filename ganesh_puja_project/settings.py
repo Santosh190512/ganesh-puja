476c2745a108a6b185e595c4ea438c6ab148389b
Santosh190512 <santoshbehera34514@gmail.com>
2026-08-29 16:35:26 +0530

    enabled debug mode for deployment troubleshooting

--- ganesh_puja_project/settings.py
+++ ganesh_puja_project/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(ev9#d&v5cnyz+s9i$jun$g!6o6lnm&!sv=uzlco%&3=h%(&r='
 
-import os
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
