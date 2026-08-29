d37b2a00ab85c440c596bde576d9b6376025821c
Santosh190512 <santoshbehera34514@gmail.com>
2026-08-29 15:48:00 +0530

    auto database migration and admin setup

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "from puja.models import CustomUser; CustomUser.objects.filter(username='admin').exists() or CustomUser.objects.create_superuser('admin', 'admin@example.com', 'admin123', role='SUPER_ADMIN')"
